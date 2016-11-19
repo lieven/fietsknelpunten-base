1b25ededd1fb1b1785f3bb87747eb5d274778ed9
Lieven Dekeyser <lievendekeyser@gmail.com>
2016-11-19 23:33:38 +0100

    Add INVALID_PARAMETER

--- ApiModule.php
+++ ApiModule.php
@@ -10,7 +10,8 @@ class ApiException extends Exception
 	const UNKNOWN_ERROR = 1;
 	const UNKNOWN_ACTION = 2;
 	const PARAMETER_REQUIRED = 3;
-	const PERMISSION_DENIED = 4;
+	const INVALID_PARAMETER = 4;
+	const PERMISSION_DENIED = 5;
 	
 	public function __construct($inErrorCode, $inMessage = '')
 	{
